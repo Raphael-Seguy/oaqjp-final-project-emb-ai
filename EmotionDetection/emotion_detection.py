1f552516c900fbb1ea87cdd3f1f3c4759ddd0006
Raphael-Seguy <raph.seguy0@gmail.com>
2024-09-19 05:22:38 -0400

    Added Unittester for the package

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -6,19 +6,27 @@ def emotion_detector(text_to_analyse):
     myheaders = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     myjson = { "raw_document": { "text": text_to_analyse } }
 
-    emotion_response = requests.post(url, json=myjson, headers=myheaders)
-    formated_emotion_response = json.loads(emotion_response.text)
+    if(text_to_analyse != "" and text_to_analyse != None):
+        emotion_response = requests.post(url, json=myjson, headers=myheaders)
+        formated_emotion_response = json.loads(emotion_response.text)
 
-    emotions_list = formated_emotion_response['emotionPredictions'][0]['emotion']
+        emotions_list = formated_emotion_response['emotionPredictions'][0]['emotion']
 
-    anger=emotions_list['anger']
-    disgust=emotions_list['disgust']
-    fear=emotions_list['fear']
-    joy=emotions_list['joy']
-    sadness=emotions_list['sadness']
+        anger=emotions_list['anger']
+        disgust=emotions_list['disgust']
+        fear=emotions_list['fear']
+        joy=emotions_list['joy']
+        sadness=emotions_list['sadness']
 
-    dominant_emotion = determine_dominant_emotion(emotions_list)
-    
+        dominant_emotion = determine_dominant_emotion(emotions_list)
+    else:
+        anger=0
+        disgust=0
+        fear=0
+        joy=0
+        sadness=0
+
+        dominant_emotion = "None"
 
     return {"anger" : anger, "disgust" : disgust, "fear" : fear, "joy" : joy, "sadness" : sadness, "dominant_emotion" : dominant_emotion}
 
